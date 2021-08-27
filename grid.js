90940b18181971f2e8b94bc578d8a5629ecdbd22
ggpwn <88844581+ggpwn@users.noreply.github.com>
2021-08-27 14:26:26 +0800

    Update grid.js

--- grid.js
+++ grid.js
@@ -3001,3 +3001,19 @@ ImagesLoaded.makeJQueryPlugin();
 return ImagesLoaded;
 
 });
+
+
+// external js: masonry.pkgd.js, imagesloaded.pkgd.js
+
+var grid = document.querySelector('.grid');
+var msnry;
+
+imagesLoaded(grid, function () {
+  // init Isotope after all images have loaded
+  msnry = new Masonry(grid, {
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true });
+
+});
+//# sourceURL=pen.js
